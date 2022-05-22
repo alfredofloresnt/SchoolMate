637d6f653172fbed5b433fa331d734b6455f87f7
Alfredo Flores <alfredofloresn.t@gmail.com>
2022-05-22 15:58:04 -0500

    Added binding configuration

--- app/src/main/java/com/example/shchoolmate/MainActivity.kt
+++ app/src/main/java/com/example/shchoolmate/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.shchoolmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shchoolmate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
