7442595071a9f9d99dda68ef460eda12f9e6a095
Alfredo Flores <alfredofloresn.t@gmail.com>
2022-05-22 16:32:38 -0500

    Added 3 views

--- app/src/main/java/com/example/shchoolmate/MainActivity.kt
+++ app/src/main/java/com/example/shchoolmate/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.shchoolmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.shchoolmate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        var navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigation,navController)
     }
 }
