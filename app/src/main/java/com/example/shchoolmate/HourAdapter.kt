fe3981cc059f9c54ef2290cc77dca11ed96f01d5
zJuarez <mji2000.741@gmail.com>
2022-05-25 20:50:49 -0500

    added ability to render a course

--- app/src/main/java/com/example/shchoolmate/HourAdapter.kt
+++ app/src/main/java/com/example/shchoolmate/HourAdapter.kt
@@ -1,8 +1,6 @@
 package com.example.schoolmate
 
 import android.graphics.Color
-import android.icu.util.Calendar
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -24,7 +22,20 @@ class HourAdapter (var hours: List<Hour>): RecyclerView.Adapter<HourAdapter.View
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.apply {
             val i = hours[position].hour
-            hourText.text = "$i:00"
+            hourText.text = if (i < 10) "0$i:00" else "$i:00"
+            if(i == 10){
+                courseAbove.setText("Programacion Avanzada")
+                courseBelow.setText("")
+                courseAbove.setBackgroundColor(Color.rgb(255	,221	,110))
+                courseBelow.setBackgroundColor(Color.rgb(255	,221	,110))
+                divider.setBackgroundColor(Color.rgb(255	,221	,110))
+            }else{
+                courseAbove.setText("")
+                courseBelow.setText("")
+                courseAbove.setBackgroundColor(Color.TRANSPARENT)
+                courseBelow.setBackgroundColor(Color.TRANSPARENT)
+                divider.setBackgroundColor(Color.LTGRAY)
+            }
         }
     }
 
